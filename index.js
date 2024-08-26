b6dd2c3a172a1c6b64d9491b1805c49ef268f032
Techno <elijahprosperie.school@gmail.com>
2024-08-26 09:29:46 -0700

    day 3 of 100 days to code

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const text = document.getElementById("text")
-let submissionAge = document.getElementById("age")
-const btnSubmit = document.getElementById("enter")
-const familiar = document.getElementById("boolean")
-let age;
+const checkBox = document.getElementById("paymentType")
+const visa = document.getElementById("visa")
+const masterCard = document.getElementById("masterCard")
+const cash = document.getElementById("cash")
 
+const payBtn = document.getElementById("submit")
+const displayChoice = document.getElementById("paymentChoice")
+const finalize = document.getElementById("endResult")
 
-
-
-btnSubmit.onclick = function(){
-    age = submissionAge.value
-    age = Number(age)
-    console.log(age)
-    if(age > 18){
-       
-        text.textContent = "Hey. Welcome!"
+payBtn.onclick = function(){
+    if(checkBox.checked){
+        displayChoice.textContent = "Great!"
+    } else{
+        displayChoice.textContent = "Please choose cash or card."
     }
-     else {
-        text.textContent = "Please leave."
-    } 
+    if(visa.checked){
+        finalize.textContent = "Great. We cover Visa!"
+    } else if(masterCard.checked){
+        finalize.textContent = "Awesome! We cover MasterCard!"
+    } else if(cash.checked) {
+        finalize.textContent = "Sure, we accept Cash as well!"
+    } else{
+        finalize.textContent = "Please choose from one of the above payment choices."
+    }
 }
